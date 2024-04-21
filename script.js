5a3e26414ffbf5811bb410233525a916e9c493ff
Ala Hojat Jalali <alahojatjalali@gmail.com>
2024-04-21 18:38:05 +0200

    🔥 clean up not used code

--- script.js
+++ script.js
@@ -8,23 +8,20 @@ gsap.to(spinningCircle, {
   transformOrigin: "center",
 });
 
-// Target the SVG element
 const pageTitle = document.querySelector("#page_title1");
 
-// Define the pulsating animation
 const pulsate = () => {
   gsap.fromTo(
     pageTitle,
-    { scale: 1 }, // Start with the element's original scale
+    { scale: 1 },
     {
-      scale: 1.05, // Scale up to 110% of the original size
-      duration: 2, // Duration of the scaling up animation
-      ease: "power1.inOut", // Easing function for smooth in and out motion
-      yoyo: true, // Revert back to the original scale after reaching 110%
-      repeat: -1, // Repeat the animation indefinitely
+      scale: 1.05,
+      duration: 2,
+      ease: "power1.inOut",
+      yoyo: true,
+      repeat: -1,
     }
   );
 };
 
-// Start the pulsating animation
 pulsate();
